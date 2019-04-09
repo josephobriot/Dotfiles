9da611a60d8cea3c277030fbc9ec843af22ed9c6
joseph <joseph.obriot@protonmail.com>
2019-04-09 18:21:56 +0200

    some changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,5 +4,6 @@ export PATH=$HOME/bin:/usr/local/bin:$HOME/.local/bin:$PATH
 
 export EDITOR='vim'
 export VISUAL='vim'
+export FZF_DEFAULT_COMMAND='find . -type f -not -path \'*/\.git/*\''
 
 fish_vi_key_bindings
