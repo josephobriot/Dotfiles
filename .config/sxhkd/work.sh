a2a551e0b1476b768fce31cd3fc2949ef1eabc9b
joseph <joseph.obriot@protonmail.com>
2019-06-28 00:40:00 +0200

    fix polybar with overlapping borders

--- .config/sxhkd/work.sh
+++ .config/sxhkd/work.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-BW=$(bspc config border_width)
-
-if [ "$2" -eq 0 ]
-then
-	bspc config window_gap -$BW
-	for side in top right bottom left ; do
-		bspc config ${side}_padding $BW
-	done
-else
-	bspc config window_gap $2;
-	for side in top right bottom left ; do
-		bspc config ${side}_padding 0
-	done
-fi
-
 reg=":a;N;\$!ba;s/\\n/;/g;s/\\[bar\\/$1\\]/&\\n/;s/.*\\n//;s/\\[.*\\]/\\n&/;s/\\n.*//;s/;/\\n/g"
 echo $reg
 
@@ -33,7 +18,22 @@ echo $borderT
 totalH=$(($height + $borderB + $borderT))
 echo $totalH
 
-bspc config top_padding $totalH;
+BW=$(bspc config border_width)
+
+if [ "$2" -eq 0 ]
+then
+	bspc config window_gap -$BW
+	for side in right bottom left ; do
+		bspc config ${side}_padding $BW
+	done
+	bspc config top_padding $(($totalH + $BW));
+else
+	bspc config window_gap $2;
+	for side in right bottom left ; do
+		bspc config ${side}_padding 0
+	done
+	bspc config top_padding $totalH;
+fi
 
 killall polybar;
 
