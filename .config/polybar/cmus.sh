6747856789cd392feaf9597d0f71fc19339fa5b6
joseph <joseph.obriot@protonmail.com>
2020-01-09 22:36:11 +0100

    cmus in bar update

--- .config/polybar/cmus.sh
+++ .config/polybar/cmus.sh
@@ -5,18 +5,18 @@ prepend_zero () {
 }
 
 artist=$(echo -n $(cmus-remote -C status | grep "tag artist" | cut -c 12-))
+song=$(echo -n $(cmus-remote -C status | grep title | cut -c 11-))
+
+if [ -z "$artist$song" ]; then
+	echo
+	exit
+fi
 
 if [ -z "$artist" ]; then
-	echo
-else
-	song=$(echo -n $(cmus-remote -C status | grep title | cut -c 11-))
-	# position=$(cmus-remote -C status | grep position | cut -c 10-)
-	# minutes1=$(prepend_zero $(($position / 60)))
-	# seconds1=$(prepend_zero $(($position % 60)))
-	# duration=$(cmus-remote -C status | grep duration | cut -c 10-)
-	# minutes2=$(prepend_zero $(($duration / 60)))
-	# seconds2=$(prepend_zero $(($duration % 60)))
-	# echo -n "$artist - $song [$minutes1:$seconds1/$minutes2:$seconds2]"
-	echo -n "$artist - $song"
+	artist="∅"
+elif [ -z "$song" ]; then
+	artist="unknown song"
 fi
+
+echo -n "$artist - $song"
 
